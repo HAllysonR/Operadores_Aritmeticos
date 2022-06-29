672d787e77706ab15c9845bcfd47d4c6a3e6faca
hallight <hallysonrobson@gmail.com>
2022-06-29 01:27:25 -0300

    operadores

---
+++ op_aritmeticos.js
@@ -0,0 +1,19 @@
+var var1 = 10
+var var2 = 8
+document.write('Soma: ' + (var1 + var2) + '<br \> ' )
+document.write('Subtração: ' + (var1 - var2) + '<br \> ')
+document.write('Multiplicação: ' + (var1 * var2) + '<br \> ') 
+document.write('Divisão: ' + (var1 / var2)  + '<br \>' )
+document.write('Módulo: ' + (var1 % var2)  + '<br \>' )
+document.write('Incremento: ' + (var1++) + '<br \>')
+document.write('pós incremento aplicado em var1: ' + (var1) + '<br \>')
+document.write('pré-decremento: ' + (--var1) + '<br\>')
+document.write('pós-decremento: ' + (var1--) + '<br\>')
+document.write('Resultado do pós-decremento: ' + (var1))
+document.write('<br \> <br \> <br \>')
+document.write('--------------------- <br\>')
+
+
+var teste = 10 
+teste %=3 // -=, /=, *=, %=
+document.write(teste)
